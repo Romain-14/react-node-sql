a1289f7ab21f4edbf209af9d034f4025323ec6e2
Romain-14 <romain14fmsn.com>
2023-09-20 00:04:15 +0200

    ready

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || process.env.LOCAL_PORT;
 
 app.use(express.static("public"));
-app.use(express.static("../../client/build"));
+app.use(express.static("../client/build"));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(router);
 
 app.get("*", (req, res) => {
-    res.sendFile("../../client/build", "index.html");
+    res.sendFile("../client/build", "index.html");
 });
 
 app.listen(PORT, () => console.log(`running on http://localhost:${PORT}`));

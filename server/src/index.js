fee259940bf60fbb1a3e5ad0769abcb9fcb06481
Romain-14 <romain14fmsn.com>
2023-09-20 12:15:11 +0200

    hotfix routes back

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || process.env.LOCAL_PORT;
 
 app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(router);
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`running on http://localhost:${PORT}`));

1e2a6d8b131952f902aa7f90cd03a554f0183c02
Romain-14 <romain14fmsn.com>
2023-09-20 11:00:45 +0200

    hotfix routes back

--- server/src/router/index.routes.js
+++ server/src/router/index.routes.js
@@ -9,8 +9,8 @@ router.use("/api/v1/tea", tea_routes);
 router.use("/api/v1/user", user_routes);
 router.use("/api/v1/category", category_routes);
 
-router.get("*", (req, res) => {
-    res.status(404).json({ msg: "not found" });
-});
+// router.get("*", (req, res) => {
+//     res.status(404).json({ msg: "not found" });
+// });
 
 export default router;
